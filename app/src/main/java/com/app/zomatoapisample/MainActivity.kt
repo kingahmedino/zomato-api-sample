98991d56ed09f7108637d73b5be59c2d1f1fb24c
kingahmedino <ahmedino001>
2020-09-14 22:42:15 +0100

    Associated main activity with recycler view

--- app/src/main/java/com/app/zomatoapisample/MainActivity.kt
+++ app/src/main/java/com/app/zomatoapisample/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.zomatoapisample.adapters.MainRecyclerAdapter
 import com.app.zomatoapisample.interfaces.MainActivityVMListener
 import com.app.zomatoapisample.models.LocationInfo
 import com.app.zomatoapisample.models.Restaurant
@@ -24,12 +26,18 @@ class MainActivity : AppCompatActivity(), MainActivityVMListener{
     }
 
     override fun onRestaurantResponseSuccessful(mutableLiveData: MutableLiveData<MutableList<Restaurant>>) {
-        mutableLiveData.observe(this, Observer {
-            Toast.makeText(this, it[0].name, Toast.LENGTH_SHORT).show()
+        mutableLiveData.observe(this, Observer { restaurants ->
+            mainActivityRV.also {
+                it.layoutManager =
+                    LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                it.setHasFixedSize(true)
+                it.adapter = MainRecyclerAdapter(restaurants, this)
+            }
         })
     }
 
     override fun onStarted() {
+        mainActivityRV.visibility = View.INVISIBLE
         mainActivityPB.visibility = View.VISIBLE
         textView.visibility = View.VISIBLE
     }
@@ -39,6 +47,7 @@ class MainActivity : AppCompatActivity(), MainActivityVMListener{
 
         mainActivityPB.visibility = View.INVISIBLE
         textView.visibility = View.INVISIBLE
+        mainActivityRV.visibility = View.VISIBLE
     }
 
     override fun onFailure() {

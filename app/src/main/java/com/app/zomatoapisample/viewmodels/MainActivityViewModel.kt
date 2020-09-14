687992eea74ad130da2d6a143707fe0b13dae90b
kingahmedino <ahmedino001>
2020-09-14 10:27:33 +0100

    Added progress bar and viewmodel

---
+++ app/src/main/java/com/app/zomatoapisample/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,49 @@
+package com.app.zomatoapisample.viewmodels
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MainActivityViewModel(context: Context): ViewModel() {
+    val requestPermissionCode = 1
+    var mLocation: Location? = null
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private val mContext = context
+
+    fun getUserLocation(){
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(mContext)
+        getLastLocation()
+    }
+
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermission()
+        } else {
+            mFusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    mLocation = location
+                    if (location != null) {
+
+                    }
+                }
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            mContext as Activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            requestPermissionCode
+        )
+    }
+}

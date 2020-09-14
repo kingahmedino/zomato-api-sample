fee58c64ce04ef0a4d1994e781a5e169a24487cf
kingahmedino <ahmedino001>
2020-09-14 12:16:54 +0100

    Added Restaurant model

---
+++ app/src/main/java/com/app/zomatoapisample/models/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.app.zomatoapisample.models
+
+class Restaurant(var id: String,
+                 var address: String,
+                 var city: String,
+                 var longitude: Double,
+                 var latitude: Double,
+                 var locality: String) {
+}

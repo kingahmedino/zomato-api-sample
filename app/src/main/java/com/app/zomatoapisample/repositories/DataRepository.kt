76e702f781607cc22a6c5bdc13a4a3c4b3ec37c2
kingahmedino <ahmedino001>
2020-10-30 23:07:59 +0100

    Added condition if response is not successful

--- app/src/main/java/com/app/zomatoapisample/repositories/DataRepository.kt
+++ app/src/main/java/com/app/zomatoapisample/repositories/DataRepository.kt
@@ -16,16 +16,18 @@ object DataRepository {
     private const val TAG = "DataRepository"
     private var mDataRepoListener: DataRepoListener? = null
 
-    fun setDataRepoResponseListener(dataRepoListener: DataRepoListener){
+    fun setDataRepoResponseListener(dataRepoListener: DataRepoListener) {
         mDataRepoListener = dataRepoListener
     }
 
-    fun getRestaurants(query: String){
+    fun getRestaurants(query: String) {
         val mutableLiveResponse = MutableLiveData<MutableList<Restaurant>>()
 
-        BackEnd().getRestaurants("1b3c8b37ea96785391fa55c288ac385c",
-            LocationInfo.latitude, LocationInfo.longitude, query,50)
-            .enqueue(object: Callback<ResponseBody>{
+        BackEnd().getRestaurants(
+            "1b3c8b37ea96785391fa55c288ac385c",
+            LocationInfo.latitude, LocationInfo.longitude, query, 50
+        )
+            .enqueue(object : Callback<ResponseBody> {
                 override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                     Log.d(TAG, "getRestaurant: onFailure")
                     mDataRepoListener?.onFailure(t.message.toString())
@@ -35,9 +37,12 @@ object DataRepository {
                     call: Call<ResponseBody>,
                     response: Response<ResponseBody>
                 ) {
-                    if (response.isSuccessful){
-                        mutableLiveResponse.value = getRestaurantsFromJSON(response.body()?.string())
+                    if (response.isSuccessful) {
+                        mutableLiveResponse.value =
+                            getRestaurantsFromJSON(response.body()?.string())
                         mDataRepoListener?.onSuccess(mutableLiveResponse)
+                    } else {
+                        mDataRepoListener?.onFailure("An error occurred")
                     }
                 }
 
@@ -49,7 +54,7 @@ object DataRepository {
         Log.d("DataRepo", "getRestaurantsFromJSON: $string")
         val parent = JSONObject(string!!)
         val restaurantArray = parent.getJSONArray("restaurants")
-        for (i in 0 until restaurantArray.length()){
+        for (i in 0 until restaurantArray.length()) {
             val json = restaurantArray[i] as JSONObject
             val jsonObject = json.getJSONObject("restaurant")
             val id = jsonObject.getString("id")
@@ -63,7 +68,18 @@ object DataRepository {
             val longitude = locationObject.getDouble("longitude")
             Log.d(TAG, "getRestaurantsFromJSON: $featuredImage")
 
-            outputList.add(Restaurant(id, name, address, city, featuredImage, longitude, latitude, locality))
+            outputList.add(
+                Restaurant(
+                    id,
+                    name,
+                    address,
+                    city,
+                    featuredImage,
+                    longitude,
+                    latitude,
+                    locality
+                )
+            )
         }
         return outputList
     }
